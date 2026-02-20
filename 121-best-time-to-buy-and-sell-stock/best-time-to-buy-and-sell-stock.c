4fce9cc93570e7a6931787d8629b47f157bcbd9c
Devadharshini R <167199090+Deviramu2005@users.noreply.github.com>
2026-02-20 09:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (17.99%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,17 @@
+int maxProfit(int* prices, int pricesSize)
+{
+   int i,max=0,min=prices[0];
+   for(i=0;i<pricesSize;i++)
+   {
+      if(prices[i]<min)
+      {
+        min=prices[i];
+      }
+    int profit=prices[i]-min;
+    if(max<profit)
+    {
+        max=profit;
+    }
+   } 
+   return max;   
+}

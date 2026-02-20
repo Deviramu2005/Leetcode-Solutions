11f7a964e59e133cddd0a45ae209dd5bedf0ad88
Devadharshini R <167199090+Deviramu2005@users.noreply.github.com>
2026-02-20 09:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.27%) - LeetSync

---
+++ 69-sqrtx/sqrtx.c
@@ -0,0 +1,29 @@
+int mySqrt(int x)
+{
+    if(x<2) 
+    { 
+        return x;
+    }
+    long left=1;
+    long right=x/2;
+    long ans=0;
+    while(left<=right)
+    {
+        long mid=left+(right-left)/2;
+        long square=mid*mid;
+        if (square==x) 
+        {
+            return mid;
+        } 
+        else if(square < x)
+        {
+            ans=mid;          
+            left=mid+1;
+        } 
+        else 
+        {
+            right=mid-1;
+        }
+    }
+    return ans;
+}

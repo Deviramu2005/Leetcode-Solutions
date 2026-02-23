d6c049046249b6a4c9dc5ca74327c85694f0d67c
Devadharshini R <167199090+Deviramu2005@users.noreply.github.com>
2026-02-23 10:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (99.58%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.c
@@ -0,0 +1,17 @@
+int lengthOfLastWord(char* s)
+ {
+    int length=0;
+    int n=strlen(s);
+    for(int i=n-1;i>=0;i--) 
+    {
+        if(s[i]!=' ') 
+        {   
+            length++;
+        } 
+        else if(length>0) 
+        {
+            return length;
+        }
+    }
+    return length;
+}

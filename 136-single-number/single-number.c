e67140b8180cf1adfeb9a3ccc7acbd446dad0e47
Devadharshini R <167199090+Deviramu2005@users.noreply.github.com>
2026-02-23 15:59:29 +0530

    Time: 1055 ms (9.43%) | Memory: 9.5 MB (13.61%) - LeetSync

--- 136-single-number/single-number.c
+++ 136-single-number/single-number.c
@@ -1,7 +1,7 @@
-int singleNumber(int* nums, int numsSize) 
-{
-   int res=0,i,j;
-   for(i=0;i<numsSize;i++)
+int singleNumber(int* nums, int numsSize)        //int res=0;
+{                                                //for(i=0;i<numsSize;i++)                                          
+   int res=0,i,j;                                //res=res^nums[i];
+   for(i=0;i<numsSize;i++)                       // return res;
    {
      int count=0;
        for(j=0;j<numsSize;j++)
